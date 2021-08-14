61d7f8b1f1e6969c766f4824f7740e73126143a8
Radek Nowak <55695745+radek-nowak@users.noreply.github.com>
2021-08-14 12:07:46 +0200

    Update Ising_init.py

--- Ising_init.py
+++ Ising_init.py
@@ -9,16 +9,16 @@ class Hamiltonian:
         self.g = g
         self.sigma_x = np.array([[0.,1.],[1.,0,]])
         self.sigma_z = np.array([[1.,0.],[0.,-1.]])
-        self.id = np.eye(2)
+        self.one = np.eye(2)
         self.ising
         
     def ising(self,d):
         self.d = d
-        sx, sz, id = self.sigma_x, self.sigma_z, self.id
+        sx, sz, one = self.sigma_x, self.sigma_z, self.one
         nbonds = self.L
         h = []
         for i in range(nbonds):
-            bonds = -self.J*np.kron(sz,sz) - self.g*np.kron(sx,id) - self.g*np.kron(id,sx)
+            bonds = -self.J*np.kron(sz,sz) - self.g*np.kron(sx,one) - self.g*np.kron(one,sx)
             h.append(np.reshape(bonds,[d,d,d,d]))
             #h.append(bonds)
         return h
