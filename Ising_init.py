b7c4200ec217553731676e8b291c2d72f44d0b64
Radek Nowak <55695745+radek-nowak@users.noreply.github.com>
2021-08-24 21:23:21 +0200

    Update Ising_init.py

--- Ising_init.py
+++ Ising_init.py
@@ -1,46 +1,43 @@
 import numpy as np
-import tensorflow as tf
+from ncon import ncon
 
 class Hamiltonian:
 
-    def __init__(self, L, J, g):
+    def __init__(self, L, J):
         self.L = L
         self.J = J
         self.sigma_z = np.array([[1.,0.],[0.,-1.]])
         self.one = np.eye(2)
-        self.tensor_L        
-        self.ising
+        #self.tensor_L    
+        #self.ising
 
     # tensor L should have 3 indices
     def tensor_L(self, beta):
         sz, one = self.sigma_z, self.one
-        return [one*np.sqrt(np.cosh(beta)), sz*np.sqrt(np.sinh(beta))]
+        return np.array([one*np.sqrt(np.cosh(beta)), sz*np.sqrt(np.sinh(beta))])
     
     # needs testing
     # Ls commute
     # this is for corner tensor
-    def tensor_T(beta):
-        L1, L2 = tensor_L(beta), tensor_L(beta)
-        L3, L4 = tensor_L(beta)[0], tensor_L(beta)[0]
-        L12 = tf.einsum('ijk, lkm -> ijlm', L1, L2) # i and l correspond do alpha
-        L123 = tf.einsum('ijlm, mb -> ijlb', L12, L3) 
-        T = tf.einsum('ijlb, bc -> ijlc', L123, L4)
+    def tensor_T(self, beta):
+        tensor_L = self.tensor_L(beta)
+        index_order_1 = [[-1,-2,1],[1,-3,-4]]
+        index_order_2 = [[-1,-2,1,-3],[1,-4,-5,-6]]
+        L1, L2 = tensor_L, tensor_L
+        L3, L4 = tensor_L, tensor_L
+        L12 = ncon([L1,L2],index_order_1)
+        L34 = ncon([L3,L4],index_order_1)
+        T = ncon([L12,L34], index_order_2)
         return T
 
-    def tensor_O(beta):
-        O = tf.einsum('ijlc')
+    def tensor_O(self, beta):
+        T = self.tensor_T(beta)
+        return ncon(T,[1,-1,-2,-3,-4,1])
         
 
-    def ising(self,d):
-        self.d = d
-        sz, one = self.sigma_z, self.one
-        sz = self.sigma_z
-        nbonds = self.L
-        
-        for i in range(nbonds):
-            bonds = -self.J*np.kron(sz,sz)
-            h.append(np.reshape(bonds,[d,d,d,d]))
-        return h
-        #test change
-    
-
+    def ising(self, beta):
+        n_sites = self.L
+        H = []
+        for i in range(n_sites):
+            H.append(self.tensor_O(beta))
+        return np.array(H)
