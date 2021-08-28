d16fcad797eb89c08e4b492972cf8e0609891c65
Radek Nowak <55695745+radek-nowak@users.noreply.github.com>
2021-08-28 14:13:21 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -13,7 +13,6 @@ tcz = lambda x: H(1,1).tensor_Z(x,'corner')
 tez = lambda x: H(1,1).tensor_Z(x,'edge')
 toz = lambda x: H(1,1).tensor_Z(x,'o')
 
-
 def sum_edge(beta,n,q):
     chi = 2
     if q == 'O':
@@ -22,10 +21,13 @@ def sum_edge(beta,n,q):
         indices = [[-1,1,-2],[-3,-5,-4,1]]
         if n == 1:
             m1 = ncon([e,o],indices).reshape(chi**2,chi,chi**2)
-            return m1
+            frob_norm_m1 = np.sqrt(ncon(m1.reshape(chi**2,chi**3)@m1.reshape(chi**2,chi**3).transpose(1,0),[1,1]))
+            return m1/frob_norm_m1
         else:
             mn = ncon([sum_edge(beta,n-1,'O'),o],indices).reshape(chi**(n+1),chi,chi**(n+1))
-            return mn
+            frob_norm_mn = np.sqrt(ncon(
+                            mn.reshape(chi**(n+1),chi**(n+2))@mn.reshape(chi**(n+1),chi**(n+2)).transpose(1,0),[1,1]))
+            return mn/frob_norm_mn
     else:
         e = tez(beta)
         o = toz(beta)
@@ -35,7 +37,9 @@ def sum_edge(beta,n,q):
             return m1
         else:
             mn = ncon([sum_edge(beta,n-1,'Z'),o],indices).reshape(chi**(n+1),chi,chi**(n+1))
-            return mn
+            frob_norm_mn = np.sqrt(ncon(
+                            mn.reshape(chi**(n+1),chi**(n+2))@mn.reshape(chi**(n+1),chi**(n+2)).transpose(1,0),[1,1]))
+            return mn/frob_norm_mn
     
 def sum_corner(beta,n,q):
     chi = 2
@@ -46,12 +50,14 @@ def sum_corner(beta,n,q):
         indices = [[1,2],[-1,3,1],[2,4,-4],[3,-2,-3,4]]
         if n == 1:
             m1 = ncon([c,e1,e2,o],indices).reshape(chi**2,chi**2)
-            return m1      
+            frob_norm_m1 = np.sqrt(ncon([m1 @ m1.transpose(1,0)],[1,1]))
+            return m1/frob_norm_m1      
         else:
             mn = ncon(
                 [sum_corner(beta,n-1,'O'),sum_edge(beta,n-1,'O'),sum_edge(beta,n-1,'O'),o],
                 indices).reshape(chi**(n+1),chi**(n+1))
-            return mn
+            frob_norm_mn = np.sqrt(ncon([mn @ mn.transpose(1,0)],[1,1]))
+            return mn/frob_norm_mn
     else:
         c = tcz(beta)
         e1,e2 = tez(beta),tez(beta)
@@ -59,55 +65,11 @@ def sum_corner(beta,n,q):
         indices = [[1,2],[-1,3,1],[2,4,-4],[3,-2,-3,4]]
         if n == 1:
             m1 = ncon([c,e1,e2,z],indices).reshape(chi**2,chi**2)
-            return m1      
+            frob_norm_m1 = np.sqrt(ncon([m1 @ m1.transpose(1,0)],[1,1]))
+            return m1/frob_norm_m1      
         else:
             mn = ncon(
-                [sum_corner(beta,n-1,'Z'),sum_edge(beta,n-1,'Z'),sum_edge(beta,n-1,'Z'),z],
-                indices).reshape(chi**(n+1),chi**(n+1))
-            return mn
-
-        
-        
-def ctmrg(beta,n, q):
-    chi = 2
-    if q == 'O':
-        c = sum_corner(beta,n,'O')
-        e = sum_edge(beta,n,'O')
-        o = to(beta)
-        
-        tensor_list1 = [e,c,e,c,e]
-        index_list1 = [[-1,-2,1],[1,2],[2,-5,3],[3,4],[-4,-3,4]]
-        t1 = ncon(tensor_list1,index_list1).reshape(4*chi**(2*n),chi**3)
-        
-        #tensor_list2 = [e,c,e,c,e]
-        #index_list2 = [[1,-2,-1],[2,1],[2,-3,3],[3,4],[4,-4,-5]]
-        #t2 = ncon(tensor_list2, index_list2).reshape(chi**3,4*chi**(2*n))
-        
-        tensor_list3 = [o,o]
-        index_list3 = [[-1,-2,-3,1],[-5,1,-6,-7]]
-        t3 = ncon(tensor_list3,index_list3).reshape(chi**3,chi**3)
-        
-        w1 = ncon([t1,t3,t1.transpose(1,0)],[[1,2],[2,3],[3,1]])
-        #trw1 = ncon(w1.reshape(4*chi**(2*n),4*chi**(2*n)),[1,1])
-        return w1#/trw1
-        #tensor_list = [t1,t3,t2]
-        #index_list = [[1,2,3,4,5],[2,3,4,6,7,8],[1,6,7,8,5]]
-        #return ncon(tensor_list,index_list)
-    else:
-        c = sum_corner(beta,n,'Z')
-        e = sum_edge(beta,n,'Z')
-        o = toz(beta)
-        
-        tensor_list1 = [e,c,e,c,e]
-        index_list1 = [[-1,-2,1],[1,2],[2,-5,3],[3,4],[-4,-3,4]]
-        t1 = ncon(tensor_list1,index_list1).reshape(4*chi**(2*n),chi**3)
-        
-        #tensor_list2 = [e,c,e,c,e]
-        #index_list2 = [[1,-2,-1],[2,1],[2,-3,3],[3,4],[4,-4,-5]]
-        #t2 = ncon(tensor_list2, index_list2).reshape(chi**3,4*chi**(2*n))
-        
-        tensor_list3 = [o,o]
-        index_list3 = [[-1,-2,-3,1],[-5,1,-6,-7]]
-        t3 = ncon(tensor_list3,index_list3).reshape(chi**3,chi**3)
-        
-        return ncon([t1,t3,t1.transpose(1,0)],[[1,2],[2,3],[3,1]])
+                    [sum_corner(beta,n-1,'Z'),sum_edge(beta,n-1,'Z'),sum_edge(beta,n-1,'Z'),z],
+                        indices).reshape(chi**(n+1),chi**(n+1))
+            frob_norm_mn = np.sqrt(ncon([mn @ mn.transpose(1,0)],[1,1]))
+            return mn/frob_norm_mn
